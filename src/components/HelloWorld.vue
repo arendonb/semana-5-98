5008f73b53e307e4d394aa863e6054f44929f685
arendonb <arendonb@gmail.com>
2020-12-21 08:24:21 -0500

    Edicion de tarjetas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -37,30 +37,40 @@ export default {
         return {
             datos: null,
             team: [{
-                codigo: 1,
-                Nombre: 'Lucas Mera',
-                Descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.',
-                Rol: 'Desarrollador backend',
-                Color: 'bg-danger'
-            }, {
-                codigo: 2,
-                Nombre: 'Cristian Sanz',
-                Descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.',
-                Rol: 'Arquictecto de software',
-                Color: 'bg-dark'
-            }, {
-                codigo: 3,
-                Nombre: 'Samara Cerezo',
-                Descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.',
-                Rol: 'Tester',
-                Color: 'bg-danger'
-            }, {
-                codigo: 4,
-                Nombre: 'Iris Mesa',
-                Descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum ac elit et accumsan.',
-                Rol: 'Analista',
-                Color: 'bg-dark'
-            }]
+          codigo: 1,
+          nombre: "Santiago Andrés Millan Pardo",
+          descripcion: "Estudiante de Ingenieria de Telecomunicaciones en la Universidad Santo Tomas. Apacionado por el deporte,desarrollo de herramientas tecnologias y la gestion de procesos. Amor por las dos ruedas.",
+          rol: "Desarrollador Backend",
+          image: "https://i.imgur.com/z4eUwdT.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "Cristhian Daniel Diaz Salazar",
+          descripcion: "Ingeniero Industrial de la Universidad Tecnológica de Pereira. Me dedico a administrar hoteles en Pereira. Apasionado por la ciencia, la tecnología y los sistemas de información. Mi pasatiempo favorito es jugar videojuegos.",
+          rol: "Scrum Master",
+          image: "https://i.imgur.com/0ULDIO2.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Miguel Enrique Marquez Aldana",
+          descripcion: "Supervisor de prospección geofísica con más de 16 años de experiencia en la planeación, supervisión y ejecución de registro de ondas sísmicas, con habilidad para el trabajo en diferentes tipos de terreno y en un ambiente multicultural.",
+          rol: "Tester",
+          image: "https://i.imgur.com/CPnYLi6.jpg?1",
+        },
+        {
+          codigo: 4,
+          nombre: "Diego Luis Soto Mendoza",
+          descripcion: "Ingeniero electrónico de la Universidad de los Llanos, estudiante de especialización en instrumentación y control industrial. Apasionado por la tecnología, la música y los videojuegos.",
+          rol: "FrontEnd",
+          image: "https://media-exp1.licdn.com/dms/image/C4E03AQHZMtBSKiBNYA/profile-displayphoto-shrink_200_200/0?e=1613001600&v=beta&t=jfWKoWUKkuFsNUV5AZGwCvOR4DZERUBJQ8AnEKzb3yw",
+        },
+        {
+          codigo: 5,
+          nombre: "Alejandro Rendón Blandón",
+          descripcion: "Ingeniero electrónico, especialista en Telecomunicaciones, especialista en Gerencia de Proyectos de la Universidad Pontificia Bolivariana en Medellín. Mi pasatiempo favorito es viajar en moto por todo el mundo, compartiendo con mis amigos.",
+          rol: "Product Owner",
+          image: "https://media-exp1.licdn.com/dms/image/C4D03AQFdkfPuDWfTbA/profile-displayphoto-shrink_200_200/0/1542999635999?e=1613001600&v=beta&t=ae7jEsQ4hQlv0B3Ql7_8OJxGBQjdzaIjvUxozVG5hFk",
+        }]
         };
     },
     
